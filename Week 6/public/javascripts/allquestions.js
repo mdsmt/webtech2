1bc24c4c69ea06d666d502cd8e602cd9ce1615e1
mdsmt <mitchell.desmet@gmail.com>
2014-03-31 16:11:09 +0200

    Like and dislike

--- Week 6/public/javascripts/allquestions.js
+++ Week 6/public/javascripts/allquestions.js
@@ -1,9 +1,27 @@
 var client = new Faye.Client('http://localhost:8000/faye');
-
+var i = 1;
 client.subscribe('/questioning', function(questioning) {
 	var naam = questioning.naam;
 	var vraag = questioning.vraag;
-	$('#allquestions').prepend('<div class="vraag"><a class="like">Like</a> <a class="dislike">Dislike</a><p class = "naam">' + naam + '</p><p class="vraag">' + vraag + '</p></div>');
+	$('#allquestionsshow').prepend('<div id="vraag' +i + '"><p class="vraag">' + vraag + '</p></div>');
   	console.log(naam + " " + vraag);
+  	i++;
 });
 
+
+
+client.subscribe('/dislikevoting', function(voting) {
+	var dislikeID = voting.dislikeID;
+	var id = '#allquestionsshow #' + dislikeID;
+	$(id).css("font-size", "-=1px");
+
+	console.log(dislikeID);
+});
+
+client.subscribe('/likevoting', function(voting) {
+	var dislikeID = voting.dislikeID;
+	var id = '#allquestionsshow #' + dislikeID;
+	$('#allquestionsshow #' + dislikeID).css("font-size", "+=1px");
+
+	console.log(dislikeID);
+});
--- Week 6/public/javascripts/ask.js
+++ Week 6/public/javascripts/ask.js
@@ -1,5 +1,5 @@
 var client = new Faye.Client('http://localhost:8000/faye');
-
+var i = 1;
 $('#verzenden').click(function(e) {
 
 	var naam = $("#txtNaam").val();
@@ -9,10 +9,32 @@ $('#verzenden').click(function(e) {
 		  naam: naam,
 		  vraag: vraag
 		});
+		$('#allquestions').prepend('<div id="vraag' +i + '"><a href= "#"class="like">Like</a> <a href="#" class="dislike">Dislike</a><p class="naam">' + naam + '</p><p class="vraag">' + vraag + '</p></div>');
+
 		$("#txtVraag").val("");
 		$("#txtNaam").val("");
 		console.log(naam + " " + vraag);
+		i++;
 	}else{
 		$('#output').text("Je naam moet minsten 3 characters lang zijn en je vraag minstens 10");
 	}
+});
+
+
+$("#allquestions").on("click","a.dislike",function(e) {
+
+	var dislikeID = $(this).parent().attr('id');
+	console.log(dislikeID);
+	client.publish('/dislikevoting', {
+		 dislikeID: dislikeID
+	});	
+});
+
+$("#allquestions").on("click","a.like",function(e) {
+
+	var dislikeID = $(this).parent().attr('id');
+	console.log(dislikeID);
+	client.publish('/likevoting', {
+		 dislikeID: dislikeID
+	});
 });
