097558d92d833b885d387f5d615c2eb7a7e68c3e
mdsmt <mitchell.desmet@gmail.com>
2014-03-31 13:40:01 +0200

    Linken naar allquestions

--- Week 6/app.js
+++ Week 6/app.js
@@ -34,6 +34,7 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/ask', question.ask);
+app.get('/allquestions', question.allQuestions);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- Week 6/routes/question.js
+++ Week 6/routes/question.js
@@ -1,3 +1,7 @@
 exports.ask = function(req, res){
   res.render('ask', { title: 'Ask a question' });
 };
+
+exports.allQuestions = function(req, res){
+  res.render('allquestions', { title: 'Ask a question' });
+};
