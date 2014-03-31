11a03809cd15869256a15224c31fb6b4e4ac3fa7
mdsmt <mitchell.desmet@gmail.com>
2014-03-31 14:29:11 +0200

    Weergeven van naam, vraag en like/dislike links.

--- Week 6/public/javascripts/allquestions.js
+++ Week 6/public/javascripts/allquestions.js
@@ -3,5 +3,7 @@ var client = new Faye.Client('http://localhost:8000/faye');
 client.subscribe('/questioning', function(questioning) {
 	var naam = questioning.naam;
 	var vraag = questioning.vraag;
+	$('#allquestions').prepend('<div class="vraag"><a class="like">Like</a> <a class="dislike">Dislike</a><p class = "naam">' + naam + '</p><p class="vraag">' + vraag + '</p></div>');
   	console.log(naam + " " + vraag);
 });
+
--- Week 6/public/javascripts/ask.js
+++ Week 6/public/javascripts/ask.js
@@ -4,9 +4,15 @@ $('#verzenden').click(function(e) {
 
 	var naam = $("#txtNaam").val();
 	var vraag = $("#txtVraag").val();
-	client.publish('/questioning', {
-	  naam: naam,
-	  vraag: vraag
-	});
-	console.log(naam + " " + vraag);
+	if(naam.length >= 3 && vraag.length >= 10){
+		client.publish('/questioning', {
+		  naam: naam,
+		  vraag: vraag
+		});
+		$("#txtVraag").val("");
+		$("#txtNaam").val("");
+		console.log(naam + " " + vraag);
+	}else{
+		$('#output').text("Je naam moet minsten 3 characters lang zijn en je vraag minstens 10");
+	}
 });
